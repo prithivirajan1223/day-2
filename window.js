3c375d8aea74b117e9c5224baa120421fda0c46f
Your Name <you@example.com>
2024-02-05 22:35:21 +0530

    first commit

---
+++ window.js
@@ -0,0 +1,13 @@
+//document 
+       //document is a object inside the window object 
+      /* we use document object for manipulation inside the document
+      /*first thing that gets loadedinto the browser is  window and properrtie
+       * we can acceses the element in the http page
+        With the help of document objects, we can add dynamic content to our web page. 
+      The document object can be accessed with a window.document or just document.
+
+     // window
+      /*The window object is the topmost object of the DOM hierarchy. 
+      It represents a browser window or frame that displays the contents of the webpage.
+       Whenever a window appears on the screen to display the contents of the document,
+        the window object is created. */
